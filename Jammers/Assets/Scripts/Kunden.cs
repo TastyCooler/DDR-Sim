aa6e049e0f1c085faaf9a574b571fb9ad0964178
Edward <Cibermon@web.de>
2018-03-15 11:31:31 +0100

    Initial commit

---
+++ Jammers/Assets/Scripts/Kunden.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kunden : MonoBehaviour {
+    public string name = "robert";
+    public int reputation = 50;
+
+    [Range(7f,11f)] public float toleranz = Random.Range(7f,11f);
+
+    public void KundeGenerieren()
+    {
+         string Setname = name;
+         int Setreputation = reputation;
+         float SetToleranz = toleranz;
+
+    }
+}
---
+++ Jammers/Assets/Scripts/Mathe.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mathe : MonoBehaviour {
+
+    Kunden f;
+
+	// Use this for initialization
+	void Start () {
+        f = GetComponent<Kunden>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Test();
+        
+	}
+
+    void Test()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            f.KundeGenerieren();
+            //Debug.LogFormat("Kunde {0} ist gekommen, *rep*={1}, Mark toleranz {2}", f.name, f.reputation, f.toleranz);
+        }
+    }
+}
