ed011b79c3dd4e37e27faa33e14cb4e03e678305
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2018-03-19 23:46:44 +0100

    no commits

--- Jammers/Assets/Scripts/Henry progress/DragAndDrop.cs
+++ Jammers/Assets/Scripts/Henry progress/DragAndDrop.cs
@@ -22,10 +22,10 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDr
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Saving parent position(Inventory) where it came from
-        returnToParent = this.transform.parent;
+        returnToParent = transform.parent;
 
         // Set object out of parent position(Inventory), however, still in canves
-        this.transform.SetParent(this.transform.parent.parent);
+        transform.SetParent(transform.parent.parent);
 
         // Stop canves object from blocking the raycast coming from mouse
         blockRay = false;
@@ -39,7 +39,7 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDr
     public void OnDrag(PointerEventData eventData)
     {
         // Set canves object to mouseposition
-        this.transform.position = eventData.position;
+        transform.position = eventData.position;
     }
 
     /// <summary>
@@ -49,7 +49,7 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler,IEndDr
     public void OnEndDrag(PointerEventData eventData)
     {
         // Set position back to parent position(Inventory)
-        this.transform.SetParent(returnToParent);
+        transform.SetParent(returnToParent);
 
         // Enable canves object to block raycast
         blockRay = true;
--- Jammers/Assets/Scripts/Henry progress/DropOff.cs
+++ Jammers/Assets/Scripts/Henry progress/DropOff.cs
@@ -8,12 +8,23 @@ public class DropOff : MonoBehaviour, IDropHandler {
     // Reference to DragAndDrop script
     DragAndDrop drop;
 
+    // Need to save location to set it back when someone put another object on Table
+    [HideInInspector] public Transform returnObject;
+
+    private bool occupied = false;
+
     public void OnDrop(PointerEventData eventData)
     {
+        // Reference to DragAndDrop pointer
         drop = eventData.pointerDrag.GetComponent<DragAndDrop>();
-        if(drop != null)
+
+        // verify if the ray/pointer hits something
+        if (drop != null)
         {
-            drop.returnToParent = this.transform;
+            returnObject = drop.returnToParent;
+
+            // Parent canves object to parent position(Table)
+            drop.returnToParent = transform;
         }
     }
 }
