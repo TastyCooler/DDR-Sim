53914b9ed0336459f2cef0763d9951550df597d7
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2018-03-19 15:15:20 +0100

    DragDrop enhanced

--- Jammers/Assets/Scripts/Henry progress/DropOff.cs
+++ Jammers/Assets/Scripts/Henry progress/DropOff.cs
@@ -5,15 +5,58 @@ using UnityEngine.EventSystems;
 
 public class DropOff : MonoBehaviour, IDropHandler {
 
+    #region Fields
+
     // Reference to DragAndDrop script
     DragAndDrop drop;
 
+    // Toggle on to let only one object on parent object(Inventory or Table)
+    public bool oneObjectOnly       = false;
+
+    // Toggle on to let more object on parent object(Inventory or Table)
+    public bool moreThenOneObject   = false;
+
+    // Parent Object(Inventory or Table) is occupied
+    private bool accessOnObject     = true;
+
+    #endregion
+
     public void OnDrop(PointerEventData eventData)
     {
+        // Reference to DragAndDrop pointer
         drop = eventData.pointerDrag.GetComponent<DragAndDrop>();
-        if(drop != null)
+
+        // Look if parent position(Table) is occupied
+        if (accessOnObject)
         {
-            drop.returnToParent = this.transform;
+            // Let objects placeable on parent object(Inventory)
+            if (moreThenOneObject)
+            {
+                if (drop != null)
+                {
+                    // place object on parent position(Inventory)
+                    drop.returnToParent = transform;
+                }
+            }
+
+            // Look if theres already an child object in the parent object(Table)
+            if (transform.childCount < 1)
+            {
+                oneObjectOnly = true;
+            }
+
+            // Let and object droppable and prohibit to drop anything if theres something
+            if (oneObjectOnly)
+            {
+                // Verify if there is an parent position(Table) to drop it off
+                if (drop != null)
+                {
+
+                    // place object on parent position(Table)
+                    drop.returnToParent = transform;
+                    oneObjectOnly = false;
+                }
+            }
         }
     }
 }
