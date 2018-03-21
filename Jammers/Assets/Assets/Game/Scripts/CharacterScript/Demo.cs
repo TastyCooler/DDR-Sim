2394519bfbc639a7518e95bca2ef5d0a55c0e0c0
TastyCooler <34125083+TastyCooler@users.noreply.github.com>
2018-03-21 21:00:33 +0100

    Up

---
+++ Jammers/Assets/Assets/Game/Scripts/CharacterScript/Demo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : Human {
+
+    public AudioClip free;
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Cola cola = collider.gameObject.GetComponent<Cola>();
+        Cigarettes cig = collider.gameObject.GetComponent<Cigarettes>();
+        Beer beer = collider.gameObject.GetComponent<Beer>();
+        Ketchup ketchup = collider.gameObject.GetComponent<Ketchup>();
+        Soup soup = collider.gameObject.GetComponent<Soup>();
+        Chocolate choc = collider.gameObject.GetComponent<Chocolate>();
+
+        if (choc && dm.waitForItem)
+        {
+
+            choc.destroyed = true;
+            dm.waitForItem = false;
+            Destroy(choc.gameObject);
+        }
+        if (cig || beer || ketchup || soup || cola && dm.waitForItem)
+        {
+            dm.wrongItem = true;
+            dm.dialogended = false;
+            AudioSource.PlayClipAtPoint(free, transform.position);
+        }
+    }
+}
--- Jammers/Assets/Assets/Game/Scripts/RegisterButtons/RedButton.cs
+++ Jammers/Assets/Assets/Game/Scripts/RegisterButtons/RedButton.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RedButton : ButtonScript {
+public class RedButton : ButtonScript
+{
+
+    private bool toggledRed;
+
+    public bool ToggledRed
+    {
+        get { return toggledRed; }
+    }
 
     public override void OnMouseDown()
     {
         sprite.sprite = targetSprite;
         mC.score--;
+        AudioSource.PlayClipAtPoint(press, transform.position);
+        toggledRed = true;
+    }
+
+    public override void OnMouseUp()
+    {
+        sprite.sprite = originSprite;
+        toggledRed = false;
     }
 }
